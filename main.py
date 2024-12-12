51911e8bd0335a100663890ba04b51dfe875ac03
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-12-12 16:41:58 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#for i in range(10):
+#    if i == 5:
+#        print(exit)
+#        exit()
+#        print(i)
+ages = [19,45,32,15]
+for age in ages:
+    if age < 18:
+        print(age, "not allowed")
+        print(exit)
+        exit()
+    else:
+        print(age, "allowed")
